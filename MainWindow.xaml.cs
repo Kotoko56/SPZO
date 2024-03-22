177fcb6f85fa1594c426bfe703ee569e7fbd21ac
Kotoko56 <kotoko5650@gmail.com>
2024-03-22 20:06:11 +0100

    initial view of main view

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,14 +11,10 @@ using System.Windows.Shapes;
 
 namespace SPZO
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() { InitializeComponent(); }
+
+
     }
 }
