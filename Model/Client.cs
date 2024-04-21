070b0435eafd506b88fccfc5e45ced9667a61c4c
Kotoko56 <kotoko5650@gmail.com>
2024-04-21 18:02:07 +0200

    binding data

--- Model/Client.cs
+++ Model/Client.cs
@@ -1,15 +1,16 @@
 ﻿namespace SPZO.Model
 {
-    class Client(int clientID, string name, string surname, string pesel, string homeAddress, string vetNumber, string phoneNumber, string rhdNumber, string arimrNumber)
+    public class Client
     {
-        public int ClientID { get; set; } = clientID;
-        public string Name { get; set; } = name;
-        public string Surname { get; set; } = surname;
-        public string Pesel { get; set; } = pesel;
-        public string HomeAddress { get; set; } = homeAddress;
-        public string VetNumber { get; set; } = vetNumber;
-        public string PhoneNumber { get; set; } = phoneNumber;
-        public string RhdNumber { get; set; } = rhdNumber;
-        public string ArimrNumber { get; set; } = arimrNumber;
+
+        public int ClientID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Pesel { get; set; }
+        public string HomeAddress { get; set; }
+        public string VetNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RhdNumber { get; set; }
+        public string ArimrNumber { get; set; }
     }
 }
--- ViewModel/ClientsViewModel.cs
+++ ViewModel/ClientsViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace SPZO.ViewModel
+﻿using SPZO.Model;
+using System.Collections.ObjectModel;
+
+namespace SPZO.ViewModel
 {
     public class ClientsViewModel : BaseViewModel
     {
+
+        private Client selectedClient;
+
+        public Client SelectedClient
+        {
+            get { return selectedClient; }
+            set
+            {
+                selectedClient = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Client> Clients { get; set; }
+
+        public ClientsViewModel()
+        {
+            Clients = new ObservableCollection<Client>();
+
+        }
     }
 }
